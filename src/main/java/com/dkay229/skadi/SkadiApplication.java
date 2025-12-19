c5e9737627388a1c7d600942df80077fd6fb3166
Danny Kay <dkay229@me.com>
2025-12-19 14:43:03 -0500

    spring

---
+++ src/main/java/com/dkay229/skadi/SkadiApplication.java
@@ -0,0 +1,13 @@
+package com.dkay229.skadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkadiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkadiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dkay229/skadi/SkadiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkay229.skadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkadiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
