bd88781f2e9fc449b6dc5f5256c4578f2c41b7d0
Danny Kay <dkay229@me.com>
2025-12-19 15:05:00 -0500

    working spring boot

---
+++ src/main/java/com/dkay229/skadi/api/PingController.java
@@ -0,0 +1,13 @@
+package com.dkay229.skadi.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "ok";
+    }
+}
